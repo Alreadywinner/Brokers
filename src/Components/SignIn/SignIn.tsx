345c63014114a9f80686b24d5889aa2016704705
Waseel Fawad <waseelfawad@gmail.com>
2021-02-21 00:30:58 +0500

    Updated the SignUp/Login UI

--- src/Components/SignIn/SignIn.tsx
+++ src/Components/SignIn/SignIn.tsx
@@ -6,31 +6,35 @@ import { Link } from "react-router-dom";
 const SignIn:React.FC = () => {
     return(
         <div>
-            <div className = "container">
-            <div className = "login-img-login">
-                <h1>Welcome to the Seller Dashboard !</h1>
-                <img src = {login} alt = "Login" /> 
-            </div>
-            <div className = "signin-form">
-                <h1 className = "main-heading">Login</h1>
-                <form>
-                    <div className = "form-grouping">
-                        <div className = "inner-grouping">
-                            <label>Email</label>
-                            <input type="email" className = "signin-input"/>
+            <div className = "main">
+                <section className = "signin">
+                    <div className = "signin-container">
+                        <div className = "signin-img">
+                            <h1>Welcome to the Seller Dashboard !</h1>
+                            <img src = {login} alt = "Login" />
                         </div>
-                        <div className = "inner-grouping">
-                            <label>Password</label>
-                            <input type="password" className = "signin-input"/>
+                        <div className = "signin-content">
+                        <h2 className = "login-heading">Login</h2>
+                        <form className = "signin-form">
+                            <div className = "form-grouping">
+                                <div className = "inner-grouping">
+                                    <label>Email</label>
+                                    <input type="email" className = "signin-input"/>
+                                </div>
+                                <div className = "inner-grouping">
+                                    <label>Password</label>
+                                    <input type="password" className = "signin-input"/>
+                                </div>
+                            </div>
+                            <div className = "term">
+                                <button className = "login-btn">Log In</button>
+                            </div>
+                            <p className = "term">Dont have an account yet? &nbsp; <Link to = "/Signup">Join Us</Link></p>
+                        </form>
                         </div>
-                    </div>
-                    <div className = "term">
-                        <button className = "login-btn">Log In</button>
-                    </div>
-                    <p className = "term">Dont have an account yet? &nbsp; <Link to = "/Signup">Join Us</Link></p>
-                </form>
+                    </div> 
+                </section>
             </div>
-        </div>
         </div>
     );
 }
--- src/Components/SignUp/SignUp.tsx
+++ src/Components/SignUp/SignUp.tsx
@@ -4,53 +4,57 @@ import login from "../../assests/login.jpg";
 import "./SignUp.css";
 const SignUp: React.FC = () => {
     return(
-        <div className = "container">
-            <div className = "login-img-login">
-                <h1>Welcome to the Seller Dashboard !</h1>
-                <img src = {login} alt = "Login" />
-            </div>
-            <div className = "main-form">
-                <h1 className = "main-heading">Register</h1>
-                <form>
-                    <div className = "form-group">
-                        <div className = "inner-group">
-                            <label>First Name</label>
-                            <input type="text"className = "signUp-input"/>
-                        </div>
-                        <div className = "inner-group">
-                            <label>Last Name</label>
-                            <input type="text" className = "signUp-input"/>
-                        </div>
+        <div className = "main">
+            <section className = "signin">
+                <div className = "signin-container">
+                    <div className = "signin-img">
+                        <h1>Welcome to the Seller Dashboard !</h1>
+                        <img src = {login} alt = "Login" />
                     </div>
-                    <div className = "form-group">
-                        <div className = "inner-group">
-                            <label>Phone Number</label>
-                            <input type="number" className = "signUp-input"/>
-                        </div>
-                        <div className = "inner-group">
-                            <label>Email</label>
-                            <input type="email" className = "signUp-input"/>
-                        </div>
+                    <div className = "signin-content">
+                        <h1 className = "login-heading">Register</h1>
+                        <form className = "signin-form">
+                            <div className = "form-group">
+                                <div className = "inner-group">
+                                    <label>First Name</label>
+                                    <input type="text"className = "signUp-input"/>
+                                </div>
+                                <div className = "inner-group">
+                                    <label>Last Name</label>
+                                    <input type="text" className = "signUp-input"/>
+                                </div>
+                            </div>
+                            <div className = "form-group">
+                                <div className = "inner-group">
+                                    <label>Phone Number</label>
+                                    <input type="number" className = "signUp-input"/>
+                                </div>
+                                <div className = "inner-group">
+                                    <label>Email</label>
+                                    <input type="email" className = "signUp-input"/>
+                                </div>
+                            </div>
+                            <div className = "form-group">
+                                <div className = "inner-group">
+                                    <label>Password</label>
+                                    <input type="password" className = "signUp-input"/>
+                                </div>
+                                <div className = "inner-group">
+                                    <label>Confirm Password</label>
+                                    <input type="password" className = "signUp-input"/>
+                                </div>
+                            </div>
+                            <div className = "terms">
+                                <input type="checkbox" className="checkbox"/>&nbsp;&nbsp;I Accept to the Terms & Conditions
+                            </div>
+                            <div className = "terms">
+                                <button className = "login-btn">Create Account</button>
+                            </div>
+                            <p className = "terms-2">Already have an account?&nbsp; <Link to = "/Brokers">Log in</Link></p>
+                        </form>
                     </div>
-                    <div className = "form-group">
-                        <div className = "inner-group">
-                            <label>Password</label>
-                            <input type="password" className = "signUp-input"/>
-                        </div>
-                        <div className = "inner-group">
-                            <label>Confirm Password</label>
-                            <input type="password" className = "signUp-input"/>
-                        </div>
-                    </div>
-                    <div className = "terms">
-                        <input type="checkbox" className="checkbox"/>&nbsp;&nbsp;I Accept to the Terms & Conditions
-                    </div>
-                    <div className = "terms">
-                        <button className = "login-btn">Create Account</button>
-                    </div>
-                    <p className = "terms-2">Already have an account?&nbsp; <Link to = "/Brokers">Log in</Link></p>
-                </form>
-            </div>
+                </div>
+            </section>
         </div>
     );
 }
